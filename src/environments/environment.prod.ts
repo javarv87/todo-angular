310d0c741556248e08b694d547c658e77b1552e6
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-10-12 10:45:46 -0500

    Set env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDTvZHi61B7tTDDsOkV5loA4v9Hfz67a5w',
+    authDomain: 'todo-angular-fs.firebaseapp.com',
+    databaseURL: 'https://todo-angular-fs.firebaseio.com',
+    projectId: 'todo-angular-fs',
+    storageBucket: '',
+    messagingSenderId: '214317744285'
+  }
 };
